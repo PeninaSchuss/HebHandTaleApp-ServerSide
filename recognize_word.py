dc851fcf7f383792636157eadb6d5fd2e184ce38
PeninaSchuss <pninast@edu.hac.ac.il>
2023-07-13 15:37:57 +0300

    changing to a generic path

--- recognize_word.py
+++ recognize_word.py
@@ -3,15 +3,19 @@ from flask import Flask
 
 from runners.run_e2e import run_e2e
 
-
 app = Flask(__name__)
+
+# Get the path to the current directory
+current_directory = os.path.dirname(os.path.abspath(__file__))
+
+# Construct the path to the data directory
+data_directory = os.path.join(current_directory, 'data')
 
 
 @app.route('/recognize_word_by_name/<image_name>')
-def recognize_word(image_name):
+def recognize_word_by_name(image_name):
     print("Image_name:", image_name)
-    input_image_dir = "/Users/stav/Projects/CodeSH/HandwritingProject/data"
-    input_image_path = os.path.join(input_image_dir, image_name + ".png")
+    input_image_path = os.path.join(data_directory, image_name + ".png")
     result = run_e2e(input_image_path)
     return {
         "words": result
@@ -19,11 +23,10 @@ def recognize_word(image_name):
 
 
 @app.route('/recognize_word_by_content/<image_content>')
-def recognize_word(image_content):
+def recognize_word_by_content(image_content):
     decode_image = None
-    image_path = decode_image(image_content) #TODO: implement
+    image_path = decode_image(image_content)  # TODO: implement
     result = run_e2e(image_path)
     return {
         "words": result
     }
-
