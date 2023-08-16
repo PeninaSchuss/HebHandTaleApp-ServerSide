b04161ce0c9269e3c580d73b82928a81df3eb272
PeninaSchuss <pninast@edu.hac.ac.il>
2023-08-16 13:22:09 +0300

    fix a bug

--- api_utils/word_img_decode.py
+++ api_utils/word_img_decode.py
@@ -8,7 +8,12 @@ from config import DIR_PATH
 def save_image_from_base64(image_content):
     image_data = base64.b64decode(image_content)
     filename = str(uuid.uuid4()) + '.png'
-    image_path = os.path.join(DIR_PATH, "base64_imgs", filename)
+
+    # Create the directory if it doesn't exist
+    save_directory = os.path.join(DIR_PATH, "base64_imgs")
+    os.makedirs(save_directory, exist_ok=True)
+
+    image_path = os.path.join(save_directory, filename)
     with open(image_path, 'wb') as image_file:
         image_file.write(image_data)
     return image_path
