d197d95b0c331d2a3d966ba3b6a09231e050689e
PeninaSchuss <pninast@edu.hac.ac.il>
2023-10-11 01:34:15 +0300

    delete an unuseful api function

--- server_api/recognize_word.py
+++ server_api/recognize_word.py
@@ -8,21 +8,6 @@ from runners.run_e2e import run_e2e
 from api_utils.google_translate_api import translate_word
 
 app = Flask(__name__)
-# Define the API URL
-
-
-@app.route('/recognize_word_by_name/<image_name>')
-def recognize_word_by_name(image_name):
-    # Get the path to the current directory
-    current_directory = os.path.dirname(os.path.abspath(__file__))
-    # Construct the path to the data directory
-    data_directory = os.path.join(current_directory, '../data')
-    print("Image_name:", image_name)
-    input_image_path = os.path.join(data_directory, image_name + ".png")
-    result = run_e2e(input_image_path)
-    return {
-        "words": result
-    }
 
 
 @app.route('/recognize_word_by_content', methods=['POST'])
